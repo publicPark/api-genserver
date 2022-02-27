d454b960a6084f9052c9285783be65fa7fa8556e
j in paradise <public.park.ji@gmail.com>
2022-02-27 20:07:39 +0900

    router

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.status(200).json({ message: "good" })
+  res.status(200).json({ message: "I'm good" })
 })
+
+const groupRouter = require('./routes/groups')
+
+app.use('/data', groupRouter)
 
 app.listen(3333, () => {
   console.log("hello 3333")
